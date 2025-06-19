0596726d5d13e5fe9ca55e319c5977e4d2a2c2ec
Saba Magradze <smagradze@cartubank.ge>
2025-06-19 18:31:48 +0400

    small changes

--- src/main/java/com/dailyhaul/config/BuildInfoController.java
+++ src/main/java/com/dailyhaul/config/BuildInfoController.java
@@ -1,13 +1,13 @@
 package com.dailyhaul.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BuildInfoController {
 
-    @Value("${build.id:default}")
+    @Value("${build.id:default}") // will load default values (application.yml build specs) if on-profile is not specified
     private String buildId;
 
     @Value("${build.version:default}")
@@ -16,6 +16,7 @@ public class BuildInfoController {
     @Value("${build.name:default}")
     private String buildName;
 
+    @GetMapping("/build-info")
     public String getBuildInfo() {
         return "Build id: " + buildId + ", version: " + buildVersion + ", name: " + buildName;
     }
