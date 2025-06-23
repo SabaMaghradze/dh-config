ff43e3f7e60ae51f31855f346f9d70f7e42af7c6
Saba Magradze <smagradze@cartubank.ge>
2025-06-23 18:20:45 +0400

    s.c.

--- src/main/java/com/dailyhaul/config/BuildInfoController.java
+++ src/main/java/com/dailyhaul/config/BuildInfoController.java
@@ -1,27 +1,31 @@
 package com.dailyhaul.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@AllArgsConstructor
 @RestController
-@AllArgsConstructor
+@RefreshScope
 public class BuildInfoController {
 
-//    @Value("${build.id:default}") // will load default values (application.yml build specs) if on-profile is not specified
-//    private String buildId;
-//
-//    @Value("${build.version:default}")
-//    private String buildVersion;
-//
-//    @Value("${build.name:default}")
-//    private String buildName;
+    @Value("${build.id:default}") // will load default values (application.yml build specs) if on-profile is not specified
+    private String buildId;
 
-    private BuildInfo buildInfo;
+    @Value("${build.version:default}")
+    private String buildVersion;
+
+    @Value("${build.name:default}")
+    private String buildName;
+
+//    private BuildInfo buildInfo;
 
     @GetMapping("/build-info")
     public String getBuildInfo() {
-        return "Build id: " + buildInfo.getId() + ", version: " + buildInfo.getVersion() + ", name: " + buildInfo.getName();
+        return "Build id: " + buildId + ", version: " + buildVersion + ", name: " + buildName;
+//        return "Build id: " + buildInfo.getId() + ", version: " + buildInfo.getVersion() + ", name: " + buildInfo.getName();
     }
 
 }
